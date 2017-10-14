4441ff4830261456b5cfaf52d7c11d1d7061d94a
Bear Hagen <b@datahjelpen.no>
2017-10-14 15:23:58 +0200

    Added crontab template

--- linux.sh
+++ linux.sh
@@ -4,9 +4,10 @@ ORIGINALDIR=$(pwd)
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-stow -vt ~ common;
-stow -vt ~ bash;
-stow -vt ~ vim;
+stow -vt ~ common
+stow -vt ~ cron
+stow -vt ~ bash
+stow -vt ~ vim
 stow -vt ~ x
 
 cd $ORIGINALDIR
