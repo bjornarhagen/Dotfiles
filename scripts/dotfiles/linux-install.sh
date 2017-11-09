53d0e4e2f4930a02660773ea8287ae32effb32e0
Bear Hagen <b@datahjelpen.no>
2017-11-09 15:41:39 +0100

    Install script now lets user to choose to install composer

--- scripts/dotfiles/linux-install.sh
+++ scripts/dotfiles/linux-install.sh
@@ -24,3 +24,27 @@ stow_load ~ vim
 stow_load ~ x
 
 printf "]\nDone!\n\n"
+
+# Install composer
+composer_installed=$(which composer | grep 'not found')
+echo "Do you wish to install this composer?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+								if [ "$composer_installed" == "not found" ]
+								then
+												echo "Starting composer install..."
+												php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+												php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+												php composer-setup.php --install-dir=bin
+												php -r "unlink('composer-setup.php');"
+												echo "Composer is now installed"
+								else
+												echo "Composer is already installed"
+								fi
+								break;;
+        No )
+								echo "Composer will not be installed."
+								exit;;
+    esac
+done
