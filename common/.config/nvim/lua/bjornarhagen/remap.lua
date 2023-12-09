f13e4b061fdbdeb916f3f45b5e9d1e9a0be8ac29
Bjørnar Hagen <bjornar@datahjelpen.no>
2023-12-09 01:21:46 +0100

    remaps for neovim, and clipboard sync

--- common/.config/nvim/lua/bjornarhagen/remap.lua
+++ common/.config/nvim/lua/bjornarhagen/remap.lua
@@ -1,2 +1,18 @@
 vim.g.mapleader = " "
+
+-- Explore Files
 vim.keymap.set("n", "<leader>ef", vim.cmd.Ex)
+
+-- Cursor movements
+vim.keymap.set("n", "gl", "$")
+vim.keymap.set("v", "gl", "$")
+
+vim.keymap.set("n", "gh", "0")
+vim.keymap.set("v", "gh", "0")
+
+vim.keymap.set("n", "gt", "gg")
+vim.keymap.set("n", "gb", "G")
+
+-- Clipboard
+-- Sync OS and vim
+vim.o.clipboard = vim.fn.has('unnamedplus') ~= 0 and 'unnamedplus' or 'unnamed'
